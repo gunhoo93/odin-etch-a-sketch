eb77fbf6001ec2f3d4bb0243c45beb131fcad4f6
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-26 09:20:40 -0400

    Add working prototype

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import { renderHtmlBoard } from './ui';
+import './style.css';
+import { BoardElement, ColorPickerElement, initializeEtchSketch } from './ui';
 import { Board } from './etch';
 
-const board = new Board({ rows: 16, columns: 16 });
-renderHtmlBoard(document.querySelector('.board-container'), board);
+initializeEtchSketch({
+    board: new BoardElement({
+        target: document.querySelector('#board'),
+        width: 960,
+        height: 960,
+        board: new Board({ rows: 16, columns: 16 })
+    }),
+    colorPicker: new ColorPickerElement({
+        target: document.querySelector('#color-input')
+    })
+});
--- src/ui.js
+++ src/ui.js
@@ -1,18 +1,70 @@
-export function renderHtmlBoard($target, board) {
-    const $tiles = [];
-    for (const [row, column] of board) {
-        const $tile = document.createElement('div');
-        $tile.className = 'tile';
-        $tile.dataset.row = row;
-        $tile.dataset.column = column;
+const Pixel = (size) => `${size}px`;
 
-        const $tileLabel = document.createElement('span');
-        $tileLabel.className = 'visually-hidden';
-        $tileLabel.textContent = `row ${row + 1} column ${column + 1}`;
-        $tile.appendChild($tileLabel);
-        $tiles.push($tile);
+export function initializeEtchSketch({ board, colorPicker }) {
+    board.listen('mouseover', (evt) => {
+        board.paint(evt.target, colorPicker.color);
+    });
+}
+
+export class BoardElement {
+    constructor({ target, width, height, board }) {
+        const tiles = [];
+        const tileWidth = width / board.columns;
+        const tileHeight = height / board.rows;
+        for (const [row, column] of board) {
+            const tile = createTileElement({
+                width: tileWidth,
+                height: tileHeight,
+                row: row,
+                column: column
+            });
+            tiles.push(tile);
+        }
+        target.style.width = Pixel(width);
+        target.style.height = Pixel(height);
+
+        // Enforces $target to contain a single board.
+        target.replaceChildren(...tiles);
+
+        this.element = target;
+        this.board = board;
     }
 
-    // Enforces $target to contain a single board.
-    $target.replaceChildren(...$tiles);
+    paint($tile, color) {
+        const { row, column } = $tile.dataset;
+        if (row === undefined || column === undefined) {
+            console.warn('Tile element missing row, column data attribute');
+        }
+        this.board.paint(row, column, color);
+        $tile.style['background-color'] = this.board.getTileColor(row, column);
+    }
+
+    listen(evtType, handler) {
+        this.element.addEventListener(evtType, handler);
+    }
+}
+
+export class ColorPickerElement {
+    constructor({ target, initialColor = '#000' }) {
+        this.color = initialColor;
+        target.addEventListener('input', (evt) => {
+            this.color = evt.target.value;
+        });
+    }
+}
+
+function createTileElement({ width, height, row, column }) {
+    const elem = document.createElement('div');
+    elem.className = 'tile';
+    elem.dataset.row = row;
+    elem.dataset.column = column;
+    elem.style.width = Pixel(width);
+    elem.style.height = Pixel(height);
+
+    const label = document.createElement('span');
+    label.className = 'visually-hidden';
+    label.textContent = `row ${row + 1} column ${column + 1}`;
+    elem.appendChild(label);
+
+    return elem;
 }
