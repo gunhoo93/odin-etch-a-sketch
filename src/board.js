6ed1846b471b1e086c9106140b8438bed248948f
Gunhoo Yoon <gunhoo93@gmail.com>
2021-09-28 00:49:35 -0400

    Refactor Board

--- src/board.js
+++ src/board.js
@@ -1,25 +1,23 @@
 export class Board {
     constructor({ target, stylus }) {
         this.target = target;
-        this.stylus = stylus;
+        this.board = document.createElement('div');
+        this.board.className = 'board';
+        this.board.style['display'] = 'grid';
+        this.board.addEventListener('mouseover', (evt) => {
+            stylus.draw(evt.target);
+        });
     }
 
     render(length) {
-        const board = document.createElement('div');
-        board.className = 'board';
-        board.style['display'] = 'grid';
-        board.style['grid-template-rows'] = `repeat(${length}, calc(100% / ${length}))`;
-        board.style['grid-template-columns'] = `repeat(${length}, calc(100% / ${length}))`;
-
+        const tiles = [];
         for (let i = 0; i < length * length; ++i) {
-            const tile = document.createElement('div');
-            board.appendChild(tile);
+            tiles.push(document.createElement('div'));
         }
-
-        board.addEventListener('mouseover', (evt) => {
-            this.stylus.draw(evt.target);
-        });
-
-        this.target.replaceChildren(board);
+        this.board.style['grid-template-rows'] = `repeat(${length}, calc(100% / ${length}))`;
+        this.board.style['grid-template-columns'] = `repeat(${length}, calc(100% / ${length}))`;
+        this.board.replaceChildren(...tiles);
+        this.target.appendChild(this.board); // appendChild acts like a replace when same element ref is passed
     }
 }
+
